d8a24b644db0a1720e0a20b05518e8b1c2535936
Eric <eric@system76-pc.localdomain>
2021-09-20 11:10:36 -0500

    add copyright and license

--- uschemecore.hh
+++ uschemecore.hh
@@ -1,3 +1,20 @@
+/*
+  The uScheme programming language
+  Copyright 2016 Eric J. Deiman
+
+  This file is part of the uScheme programming language.
+  The uScheme programming language is free software: you can redistribute it
+  and/ormodify it under the terms of the GNU General Public License as published by the
+  Free Software Foundation, either version 3 of the License, or (at your option) any
+  later version.
+  
+  The uScheme programming language is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License along with the
+  uScheme programming language. If not, see <https://www.gnu.org/licenses/>
+*/
+
 #ifndef USCHEMECORE_HH
 #define USCHEMECORE_HH
 
